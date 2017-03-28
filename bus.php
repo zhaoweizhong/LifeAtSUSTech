d6132f6b5121eca2955f7f0011bae1483dbb6c77
Zhaowei Zhong <zhong_zhaowei@163.com>
2017-03-28 18:08:02 +0800

    School Bus Timetable Finished

---
+++ bus.php
@@ -0,0 +1,219 @@
+<?php
+$pageTitle = "校园巴士时刻表&nbsp;-&nbsp;";
+$pageType = "tutorial";
+require 'includes/header.php';
+?>
+
+    <div class="wrapper">
+        <div class="header header-filter" style="background-image: url('https://cache.sustech.net/bg1.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <h1 class="title text-center">校园巴士时刻表</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="main main-raised">
+            <div class="section">
+                <div class="container">
+                    <div class="row tim-row">
+                        <div>
+
+                            <!-- Nav tabs -->
+                            <ul class="nav nav-pills" role="tablist">
+                                <li role="presentation" class="active"><a href="#workday" aria-controls="workday" role="tab" data-toggle="tab">工作日</a></li>
+                                <li role="presentation"><a href="#weekend" aria-controls="weekend" role="tab" data-toggle="tab">周末/节假日</a></li>
+                            </ul>
+
+                            <!-- Tab panes -->
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane fade in active" id="workday">
+                                    <br>
+                                    <div class="col-xs-6 .col-sm-4">
+                                        <h4>荔园</h4>
+                                        <table class="table table-striped text-center">
+                                            <tr>
+                                                <td>7:10</td>
+                                                <td>7:20</td>
+                                                <td>7:30</td>
+                                            </tr>
+                                            <tr>
+                                                <td>7:40</td>
+                                                <td>8:15</td>
+                                                <td>9:25</td>
+                                            </tr>
+                                            <tr>
+                                                <td>9:50</td>
+                                                <td>10:00</td>
+                                                <td>11:45</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12:00</td>
+                                                <td>12:15</td>
+                                                <td>12:30</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12:45</td>
+                                                <td>13:00</td>
+                                                <td>13:20</td>
+                                            </tr>
+                                            <tr>
+                                                <td>13:30</td>
+                                                <td>13:40</td>
+                                                <td>15:20</td>
+                                            </tr>
+                                            <tr>
+                                                <td>15:50</td>
+                                                <td>16:00</td>
+                                                <td>18:30</td>
+                                            </tr>
+                                            <tr>
+                                                <td>18:45</td>
+                                                <td>21:00</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div class="col-xs-6 .col-sm-4">
+                                        <h4>图书馆</h4>
+                                        <table class="table table-striped text-center">
+                                            <tr>
+                                                <td>8:30</td>
+                                                <td>9:50</td>
+                                                <td>10:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>11:45</td>
+                                                <td>12:00</td>
+                                                <td>12:15</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12:30</td>
+                                                <td>12:45</td>
+                                                <td>13:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>13:30</td>
+                                                <td>13:40</td>
+                                                <td>15:40</td>
+                                            </tr>
+                                            <tr>
+                                                <td>15:50</td>
+                                                <td>16:00</td>
+                                                <td>17:30</td>
+                                            </tr>
+                                            <tr>
+                                                <td>21:10</td>
+                                                <td>22:05</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>-</td>
+                                                <td>-</td>
+                                                <td>-</td>
+                                            </tr>
+                                            <tr>
+                                                <td>-</td>
+                                                <td>-</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div role="tabpanel" class="tab-pane fade" id="weekend">
+                                    <br>
+                                    <div class="col-xs-6 .col-sm-4">
+                                        <h4>荔园</h4>
+                                        <table class="table table-striped text-center">
+                                            <tr>
+                                                <td>7:40</td>
+                                                <td>8:20</td>
+                                                <td>8:50</td>
+                                            </tr>
+                                            <tr>
+                                                <td>9:20</td>
+                                                <td>9:50</td>
+                                                <td>11:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>11:30</td>
+                                                <td>12:00</td>
+                                                <td>14:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14:30</td>
+                                                <td>15:00</td>
+                                                <td>16:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>17:00</td>
+                                                <td>19:00</td>
+                                                <td>20:00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>20:45</td>
+                                                <td>-</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div class="col-xs-6 .col-sm-4">
+                                        <h4>图书馆</h4>
+                                        <table class="table table-striped text-center">
+                                            <tr>
+                                                <td>8:05</td>
+                                                <td>8:35</td>
+                                                <td>9:05</td>
+                                            </tr>
+                                            <tr>
+                                                <td>9:35</td>
+                                                <td>10:05</td>
+                                                <td>11:15</td>
+                                            </tr>
+                                            <tr>
+                                                <td>11:45</td>
+                                                <td>12:15</td>
+                                                <td>14:15</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14:45</td>
+                                                <td>15:15</td>
+                                                <td>16:15</td>
+                                            </tr>
+                                            <tr>
+                                                <td>17:15</td>
+                                                <td>19:15</td>
+                                                <td>20:15</td>
+                                            </tr>
+                                            <tr>
+                                                <td>21:00</td>
+                                                <td>-</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+<script>
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    $('#myTabs a[href="#workday"]').tab('show') // Select tab by name
+    $('#myTabs a:first').tab('show') // Select first tab
+    $('#myTabs a:last').tab('show') // Select last tab
+    $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+</script>
+<?php require 'includes/footer.php'; ?>

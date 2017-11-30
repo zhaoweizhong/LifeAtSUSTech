63b17b949e95a90dd7a7458f8fa095d08bc64a52
Zhaowei Zhong <zhong_zhaowei@163.com>
2017-11-30 15:00:21 +0800

    Add Google Adsense Head Code

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,14 @@
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
 	<link href="https://cache.sustech.net/assets/css/demo.css" rel="stylesheet" />
-
+	<!-- Google Adsense -->
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-6170983579457537",
+	    enable_page_level_ads: true
+	  });
+	</script>
 </head>
 
 <body class="<?php echo $pageType; ?>-page">
